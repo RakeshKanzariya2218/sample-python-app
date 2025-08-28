c59e777586cd1c8d39fb44d1d0f2ebe369c5048b
Rakesh Kanzariya. <rkanzariya1234@gmail.com>
2025-08-28 18:09:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def create_app(config_object=None):
     def index():
         return jsonify({"status": "ok", "msg": "Welcome , Rakesh kanzariya !"})
 
-    return app rakesh 
+    return app 
 
 
 if __name__ == '__main__':
